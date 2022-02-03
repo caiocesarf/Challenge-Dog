f6518b7a41de546bcf2cde08c13d0c6c9117e788
caio fernandes <fernandescaio00@gmail.com>
2022-02-03 01:20:59 -0300

    adiciionar dogs breeds dto

---
+++ app/src/main/java/com/example/dogapp/data/model/BreedsDTO.kt
@@ -0,0 +1,34 @@
+
+data class BreedsDTO(
+    val bred_for: String,
+    val breed_group: String,
+    val country_code: String,
+    val description: String,
+    val height: Height,
+    val history: String,
+    val id: Int,
+    val image: Image,
+    val life_span: String,
+    val name: String,
+    val origin: String,
+    val reference_image_id: String,
+    val temperament: String,
+    val weight: Weight
+)
+
+data class Height(
+    val imperial: String,
+    val metric: String
+)
+
+data class Image(
+    val height: Int,
+    val id: String,
+    val url: String,
+    val width: Int
+)
+
+data class Weight(
+    val imperial: String,
+    val metric: String
+)
